75c3b4fa10a5db9daabdf554558e49e59fb0fa86
Travis Tilley <ttilley@recyclebank.com>
2008-11-14 11:07:01 -0500

    workaround

--- lib/param_parser.rb
+++ lib/param_parser.rb
@@ -1,12 +1,10 @@
-unless ActionController::Base.param_parsers[Mime::JSON]
-  # assume rails 2.0, which doesn't have a param parser for JSON put/post.
-  ActionController::Base.param_parsers[Mime::JSON] = lambda do |body|
-    if body.blank?
-      {}
-    else
-      data = ActiveSupport::JSON.decode(body)
-      data = {:_json => data} unless data.is_a?(Hash)
-      data.with_indifferent_access
-    end
+# always assign json data to :_json as a workaround.
+ActionController::Base.param_parsers[Mime::JSON] = lambda do |body|
+  if body.blank?
+    {}
+  else
+    data = ActiveSupport::JSON.decode(body)
+    data = {:_json => data}
+    data.with_indifferent_access
   end
 end
